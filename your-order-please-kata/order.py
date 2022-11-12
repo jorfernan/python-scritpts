802a2677824399087bdf193680bd64db8d487949
U-JORFERNAN\Jorge <jorfernan1995@gmail.com>
2022-11-12 22:10:39 +0100

    First solution created

--- your-order-please-kata/order.py
+++ your-order-please-kata/order.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+import re
 """Takes a string as input and sorts it according to
 the number that each word contains """
 def order_sentence(sentence):
@@ -6,6 +7,22 @@ def order_sentence(sentence):
     # the input sentence is empty
     if sentence == '':
         return ''
-    # Turn input string into list
+    # Turn input string into a list
     word_list = sentence.split()
-    
+    # Size of the list stored in a variable plus 1
+    words_number = len(word_list) + 1
+    # Initializing an empty list to return
+    ans = []
+    # Loop from 1 to words_number
+    for number in range(1,words_number):
+        # Pattern to locate word with current number
+        pattern = r" *(([a-zA-Z]*)*"+str(number)+r"[a-zA-Z]*) *"
+        # Obtain word that matches current number
+        word = re.search(pattern,sentence).group(1).strip()
+        # Append new word to the final answer
+        ans.append(word)
+    # Turn ans into a string
+    return ' '.join(ans)
+
+        
+
