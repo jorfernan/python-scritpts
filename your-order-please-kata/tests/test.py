90d1d1d1bcdc2331856392946d211b159d2367f6
U-JORFERNAN\Jorge <jorfernan1995@gmail.com>
2022-11-12 21:27:09 +0100

    Added test to the beggining of each method from tests

--- your-order-please-kata/tests/test.py
+++ your-order-please-kata/tests/test.py
@@ -17,11 +17,11 @@ from order import order_sentence
 
 class testYourOrder(unittest.TestCase):
     
-    def standarUse(self):
+    def test_standard_use(self):
         self.assertEqual(order_sentence("is2 Thi1s T4est 3a"), "Thi1s is2 3a T4est")
         self.assertEqual(order_sentence("4of Fo1r pe6ople g3ood th5e the2"), "Fo1r the2 g3ood 4of th5e pe6ople")
     
-    def emptyString(self):
+    def test_empty_string(self):
         self.assertEqual(order_sentence(""), "")
 
 if __name__ == '__main__':
